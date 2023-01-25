162a8ca91587410004ab77c9df65b136c417565a
winstont14 <anandaton000@gmail.com>
2023-01-25 13:28:19 +0700

    add button

--- app.js
+++ app.js
@@ -70,4 +70,17 @@ function processQuestionnaire() {
             }
             document.getElementById("processQuestionnaire").innerHTML = "thankyou";
           }
+const $icon = document.querySelector('.icon');
+const $arrow = document.querySelector('.arrow');
+
+$icon.onclick = () => {
+  $arrow.animate([
+    {left: '0'},
+    {left: '10px'},
+    {left: '0'}
+  ],{
+    duration: 700,
+    iterations: Infinity
+  });
+}
         
